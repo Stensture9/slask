e4e0d400380cc2f115d990ffd8b63e67e80c1a00
Stensture9 <sebstiano.swe@gmail.com>
2026-09-11 10:34:08 +0200

    update 4

--- Program.cs
+++ Program.cs
@@ -1,15 +1,1 @@
-﻿namespace simpleGit;
-
-class Program
-{
-    static void Main()
-    {
-        Console.WriteLine("Whats your name?");
-        string name = Console.ReadLine();
-        Console.WriteLine(DoSomething(""));
-    }
-    static string DoSomething (string name)
-    {
-        return $"Hello {name}";
-    }
-}
+﻿

7cd79ff5cd98ea7b38e17d78125a3bfc192c6ff2
Stensture9 <sebstiano.swe@gmail.com>
2026-09-14 09:32:00 +0200

    gamer

--- Program.cs
+++ Program.cs
@@ -1,16 +1,28 @@
-﻿Console.WriteLine("gissa vad jag tänker på");
-string? strong = Console.ReadLine();
-switch(strong)
+﻿while (true)
 {
-    case "Fiske":
-        {
-            Console.WriteLine("Rätt");
-        }
-    break;
-    default:
-        {
-            Console.WriteLine("Fel");
-        }
-    break;
+    Console.WriteLine("gissa vad jag tänker på");
+    string? strong = Console.ReadLine();
+    switch(strong)
+    {
+        case "Fiske":
+            {
+                Console.WriteLine("Rätt");
+                Environment.Exit(0);
+            }
+        break;
 
+        case "x":
+            {
+                Console.WriteLine("Hejdå");
+                Environment.Exit(0);
+            }
+        break;
+
+        default:
+            {
+                Console.WriteLine("Fel");
+            }
+        break;
+    }
+continue;
 }
